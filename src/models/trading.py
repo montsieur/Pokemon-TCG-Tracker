370b865c94a1c4380f93c8e13a22cbb81f1d58df
montsieur <dndott@gmail.com>
2024-09-29 07:30:59 +1000

    editted fields for trade model to show clearer information and not just integers

--- src/models/trading.py
+++ src/models/trading.py
@@ -26,25 +26,15 @@ class Trade(db.Model):
         return f'<Trade id={self.id}>'
 
 class TradingSchema(ma.Schema):
-    id = fields.Int()
-    offering_user_id = fields.Int()
-    receiving_user_id = fields.Int()
-    offering_card_id = fields.Int()
-    receiving_card_id = fields.Int()
-    offering_quantity = fields.Int()
-    receiving_quantity = fields.Int()
-    status_id = fields.Int()
-    
     offering_user = fields.Nested('UserSchema', only=("id", "username"))
     receiving_user = fields.Nested('UserSchema', only=("id", "username"))
-    offering_card = fields.Nested('CardSchema', only=("id", "name"))
-    receiving_card = fields.Nested('CardSchema', only=("id", "name"))
+    offering_card = fields.Nested('CardSchema', only=("id", "name", "card_type"))
+    receiving_card = fields.Nested('CardSchema', only=("id", "name", "card_type"))
     status = fields.Nested('StatusSchema', only=("id", "status_name"))
 
     class Meta:
-        fields = ("id", "offering_user_id", "receiving_user_id", "offering_card_id", 
-                  "receiving_card_id", "offering_quantity", "receiving_quantity", "status")
-
+        fields = ("id", "offering_user", "receiving_user", "offering_card", 
+                  "receiving_card", "offering_quantity", "receiving_quantity", "status")
 
 # To handle a single Trade object
 trade_schema = TradingSchema()
